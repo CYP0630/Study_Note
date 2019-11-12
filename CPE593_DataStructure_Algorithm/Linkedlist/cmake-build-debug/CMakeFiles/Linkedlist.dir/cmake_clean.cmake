ad9a9f53099285838ad99f184bee6a7fb91e7f8a
CYP0630 <yupeng.cao13@gmail.com>
2019-11-11 19:46:00 -0500

    linkedlist

---
+++ CPE593_DataStructure_Algorithm/Linkedlist/cmake-build-debug/CMakeFiles/Linkedlist.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Linkedlist.dir/main.cpp.o"
+  "Linkedlist"
+  "Linkedlist.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Linkedlist.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
