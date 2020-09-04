7776460d22c05c1f94b69327a11400369f7ac029
CYP <yupeng.cao13@gmail.com>
2020-09-04 11:31:10 -0400

    update

---
+++ 1_Python/lintcode/423_valid_parentheses.py
@@ -0,0 +1,23 @@
+
+"""
+@param s: A string
+@return: whether the string is a valid parentheses
+"""
+def isValidParentheses(self, s):
+    # write your code here
+    stack = []
+    for ch in s:
+        if ch == '{' or ch == '[' or ch == '(':
+            stack.append(ch)
+        else:
+                
+            if not stack:
+                return False
+                
+            if ch == ']' and stack[-1] != '[' or ch == '}' and stack[-1] != '{' and stack[-1] != '(':
+                return False
+                
+            stack.pop()
+                
+    return not stack
+
